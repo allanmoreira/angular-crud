bb9a7081597d72f3050fda97e761c9742901d828
Allan Moreira <allanfelipe.moreira@gmail.com>
2026-04-10 21:18:52 -0300

    Menu

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCard, MatCardActions, MatCardTitle} from "@angular/material/card";
 import {RouterLink, RouterOutlet} from "@angular/router";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-root',
-  imports: [MatToolbarModule, MatButtonModule, MatCardActions, MatCardTitle, MatCard, RouterOutlet, RouterLink],
+  imports: [MatToolbarModule, MatButtonModule, MatCardActions, MatCardTitle, MatCard, RouterOutlet, RouterLink, MatIcon],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
