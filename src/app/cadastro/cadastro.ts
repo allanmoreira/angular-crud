1f2aa78eeec4cd8be2a364c82744b270dce22e06
Allan Moreira <allanfelipe.moreira@gmail.com>
2026-04-10 22:05:08 -0300

    Formulário

---
+++ src/app/cadastro/cadastro.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
+import {FormsModule} from "@angular/forms";
+import {MatFormField, MatInput, MatLabel} from "@angular/material/input";
+import {MatButton} from "@angular/material/button";
+import {MatIcon} from "@angular/material/icon";
+
+@Component({
+  selector: 'app-cadastro',
+  imports: [
+    FlexLayoutModule,
+    MatCard,
+    MatCardTitle,
+    MatCardHeader,
+    FormsModule,
+    MatCardContent,
+    MatFormField,
+    MatLabel,
+    MatInput,
+    MatButton,
+    MatCardActions,
+    MatIcon
+  ],
+  templateUrl: './cadastro.html',
+  styleUrl: './cadastro.scss',
+})
+export class Cadastro {}
