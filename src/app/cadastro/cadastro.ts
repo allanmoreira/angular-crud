95f31c605ada14981b491af0bf9ace9b3f795f87
Allan Moreira <allanfelipe.moreira@gmail.com>
2026-04-10 22:19:40 -0300

    Form + classe

--- src/app/cadastro/cadastro.ts
+++ src/app/cadastro/cadastro.ts
@@ -1,28 +1,35 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {FlexLayoutModule} from "@angular/flex-layout";
 import {MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
 import {FormsModule} from "@angular/forms";
 import {MatFormField, MatInput, MatLabel} from "@angular/material/input";
 import {MatButton} from "@angular/material/button";
 import {MatIcon} from "@angular/material/icon";
+import {Cliente} from "./cliente";
 
 @Component({
-  selector: 'app-cadastro',
-  imports: [
-    FlexLayoutModule,
-    MatCard,
-    MatCardTitle,
-    MatCardHeader,
-    FormsModule,
-    MatCardContent,
-    MatFormField,
-    MatLabel,
-    MatInput,
-    MatButton,
-    MatCardActions,
-    MatIcon
-  ],
-  templateUrl: './cadastro.html',
-  styleUrl: './cadastro.scss',
+    selector: 'app-cadastro',
+    imports: [
+        FlexLayoutModule,
+        MatCard,
+        MatCardTitle,
+        MatCardHeader,
+        FormsModule,
+        MatCardContent,
+        MatFormField,
+        MatLabel,
+        MatInput,
+        MatButton,
+        MatCardActions,
+        MatIcon
+    ],
+    templateUrl: './cadastro.html',
+    styleUrl: './cadastro.scss',
 })
-export class Cadastro {}
+export class Cadastro {
+    cliente: Cliente = Cliente.newClient();
+
+    salvar(){
+        console.log(this.cliente);
+    }
+}
